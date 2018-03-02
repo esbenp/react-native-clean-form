1a0fa4c49ae420751d42e08996c0559b2958f7c0
Alexandr Priezzhev <priezz@users.noreply.github.com>
2018-03-02 20:07:59 +0100

    Update createInputs.js
    Code optimization

--- src/redux-form/createInputs.js
+++ src/redux-form/createInputs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import {Text, View} from 'react-native'
 import {
   Field
 } from 'redux-form/immutable'
@@ -10,28 +10,19 @@ import {
 } from '../../index'
 
 const createInputs = inputCreator => {
-  const renderInput = ({ input: { onChange, ...restInput }, ...rest}) => (
+  const renderInput = ({input: {onChange, ...restInput}, ...rest}) => (
     <InputRenderer onChangeText={onChange} {...restInput} {...rest} />
   )
   const Input = inputCreator('Input', renderInput, InputRenderer.propTypes, InputRenderer.defaultProps)
 
-  const renderSelect = ({ input: { onChange, value, ...restInput }, ...rest }) => (
-    <SelectRenderer
-      onValueChange={onChange}
-      value={value}
-      {...restInput}
-      {...rest}
-    />
+  const renderSelect = ({input: {onChange, ...restInput}, ...rest}) => (
+    <SelectRenderer onValueChange={onChange} {...restInput} {...rest} />
   )
   const Select = inputCreator('Select', renderSelect, SelectRenderer.propTypes, SelectRenderer.defaultProps)
 
-  const renderSwitch = ({ input: { onChange, value, ...restInput }, ...rest}) => {
-    // redux-form default value is '', however Switch must take a boolean value
-    if (value === '') {
-      value = SwitchRenderer.defaultProps.value
-    }
-    return <SwitchRenderer onValueChange={onChange} value={value} {...restInput} {...rest} />
-  }
+  const renderSwitch = ({input: {onChange, value, ...restInput}, ...rest}) => (
+    <SwitchRenderer onValueChange={onChange} value={!!value} {...restInput} {...rest} />
+  )
   const Switch = inputCreator('Switch', renderSwitch, SwitchRenderer.propTypes, SwitchRenderer.defaultProps)
 
   return {
